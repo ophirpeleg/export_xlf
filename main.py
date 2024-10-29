33430fb449a981c8c24fc93a55d6679d6f207601
ophir <ophir.peleg1@gmail.com>
2024-10-29 10:21:32 +0200

    Fix the issue with null values

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ import tkinter as tk
 import logging
 from openpyxl.styles import Font, PatternFill, Border, Side, Alignment
 
-
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 # Global variable for output file path
 output_file_path = None
@@ -39,75 +37,8 @@ def style_excel_sheet(ws):
     ws.sheet_view.showGridLines = False
 
 
-def excel_to_xliff(excel_file):
-    try:
-        wb = openpyxl.load_workbook(excel_file)
-        logging.info("Excel workbook loaded successfully.")
-
-        for sheet_name in wb.sheetnames:
-            sheet = wb[sheet_name]
-
-            root = ET.Element("xliff", version="1.2")
-            file_attributes = {
-                "original": "Salesforce",
-                "source-language": "en_US",
-                "target-language": sheet_name,
-                "translation-type": "metadata",
-                "datatype": "xml"
-            }
-            file_element = ET.SubElement(root, "file", **file_attributes)
-            body_element = ET.SubElement(file_element, "body")
-
-            seen_ids = set()  # To keep track of seen Ids and avoid duplicates
-            for row in sheet.iter_rows(min_row=2, values_only=True):
-                id_value = str(row[0])  # Assuming the ID is in the first column
-                if id_value not in seen_ids:  # Check if the ID is already processed
-                    seen_ids.add(id_value)  # Mark this ID as seen
-                    trans_unit_element = ET.SubElement(body_element, "trans-unit", id=id_value, maxwidth=str(row[1]), size_unit=str(row[2]))
-                    source_element = ET.SubElement(trans_unit_element, "source")
-                    source_element.text = f"{str(row[3])}"
-                    target_element = ET.SubElement(trans_unit_element, "target")
-                    target_element.text = f"{str(row[4])}"
-
-                    if len(row) > 5 and row[5]:
-                        note_element = ET.SubElement(trans_unit_element, "note")
-                        note_element.text = f"{str(row[5])}"
-
-            xml_declaration = '<?xml version="1.0" encoding="UTF-8"?>\n'
-            xml_string = minidom.parseString(ET.tostring(root)).toprettyxml(indent="    ")
-            xml_string = xml_string.split('\n', 1)[1] if xml_string.startswith('<?xml') else xml_string
-
-            full_xml = xml_declaration + xml_string
-
-            output_file_path = filedialog.asksaveasfilename(
-                title="Save File As",
-                defaultextension=".xlf",
-                filetypes=[("XLIFF files", "*.xlf"), ("All Files", "*.*")],
-                initialfile=f"{sheet_name}_output.xlf"
-            )
-
-            if output_file_path:
-                with open(output_file_path, "w", encoding="utf-8") as f:
-                    f.write(full_xml)
-                logging.info(f"File saved successfully at {output_file_path}")
-                print(f"File saved successfully at {output_file_path}")
-            else:
-                logging.info("File save operation was cancelled.")
-                print("File save operation was cancelled.")
-
-        logging.info("Conversion complete.")
-        print("Conversion complete.")
-    except Exception as e:
-        logging.error(f"An error occurred during the conversion: {e}")
-        print(f"An error occurred during the conversion: {e}")
-        raise
-
-
-# Function to convert XLIFF to Excel
+# Refactored function to convert XLIFF to Excel without saving
 def xliff_to_excel(xliff_file):
-    global output_file_path  # Make output_file_path global
-    global target_language
-
     try:
         tree = ET.parse(xliff_file)
         root = tree.getroot()
@@ -143,24 +74,7 @@ def xliff_to_excel(xliff_file):
         # Apply styling to the worksheet
         style_excel_sheet(ws)
 
-        # Set default output file name using target-language
-        default_output_filename = f"Excel to xlf {target_language}.xlsx"
-
-        # Save the file and store the output path in the global variable
-        output_file_path = filedialog.asksaveasfilename(
-            title="Save Excel File As",
-            defaultextension=".xlsx",
-            filetypes=[("Excel files", "*.xlsx"), ("All Files", "*.*")],
-            initialfile=default_output_filename
-        )
-
-        if output_file_path:
-            wb.save(output_file_path)
-            logging.info(f"XLIFF converted to Excel and saved at {output_file_path}")
-            print(f"XLIFF converted to Excel and saved at {output_file_path}")
-        else:
-            logging.info("File save operation was cancelled.")
-            print("File save operation was cancelled.")
+        return wb, target_language
 
     except Exception as e:
         logging.error(f"An error occurred during the XLIFF to Excel conversion: {e}")
@@ -168,10 +82,79 @@ def xliff_to_excel(xliff_file):
         raise
 
 
+
+# Function to convert Excel to XLIFF
+def excel_to_xliff(excel_file):
+    try:
+        wb = openpyxl.load_workbook(excel_file)
+        logging.info("Excel workbook loaded successfully.")
+
+        for sheet_name in wb.sheetnames:
+            sheet = wb[sheet_name]
+
+            root = ET.Element("xliff", version="1.2")
+            file_attributes = {
+                "original": "Salesforce",
+                "source-language": "en_US",
+                "target-language": sheet_name,
+                "translation-type": "metadata",
+                "datatype": "xml"
+            }
+            file_element = ET.SubElement(root, "file", **file_attributes)
+            body_element = ET.SubElement(file_element, "body")
+
+            seen_ids = set()  # To keep track of seen Ids and avoid duplicates
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                id_value = str(row[0])  # Assuming the ID is in the first column
+                if id_value not in seen_ids:  # Check if the ID is already processed
+                    seen_ids.add(id_value)  # Mark this ID as seen
+                    trans_unit_element = ET.SubElement(body_element, "trans-unit", id=id_value, maxwidth=str(row[1]),
+                                                       size_unit=str(row[2]))
+                    source_element = ET.SubElement(trans_unit_element, "source")
+                    source_element.text = f"{str(row[3])}"
+                    target_element = ET.SubElement(trans_unit_element, "target")
+                    target_text = str(row[4]) if row[4] else "<>"  # Set "<>" if Target is None or empty
+                    target_element.text = target_text
+
+                    if len(row) > 5 and row[5]:
+                        note_element = ET.SubElement(trans_unit_element, "note")
+                        note_element.text = f"{str(row[5])}"
+
+            xml_declaration = '<?xml version="1.0" encoding="UTF-8"?>\n'
+            xml_string = minidom.parseString(ET.tostring(root)).toprettyxml(indent="    ")
+            xml_string = xml_string.split('\n', 1)[1] if xml_string.startswith('<?xml') else xml_string
+
+            full_xml = xml_declaration + xml_string
+
+            output_file_path = filedialog.asksaveasfilename(
+                title="Save File As",
+                defaultextension=".xlf",
+                filetypes=[("XLIFF files", "*.xlf"), ("All Files", "*.*")],
+                initialfile=f"{sheet_name}_output.xlf"
+            )
+
+            if output_file_path:
+                with open(output_file_path, "w", encoding="utf-8") as f:
+                    f.write(full_xml)
+                logging.info(f"File saved successfully at {output_file_path}")
+                print(f"File saved successfully at {output_file_path}")
+            else:
+                logging.info("File save operation was cancelled.")
+                print("File save operation was cancelled.")
+
+        logging.info("Conversion complete.")
+        print("Conversion complete.")
+    except Exception as e:
+        logging.error(f"An error occurred during the conversion: {e}")
+        print(f"An error occurred during the conversion: {e}")
+        raise
+
+
+# Function to select an Excel file and convert it to XLIFF
 def select_excel_to_xliff():
     excel_file_path = filedialog.askopenfilename(
         title="Select Excel File",
-        filetypes=[("Excel files", "*.xlsx;*.xls"),]
+        filetypes=[("Excel files", "*.xlsx;*.xls"), ]
     )
     if excel_file_path:
         logging.info("Excel file selected.")
@@ -182,6 +165,7 @@ def select_excel_to_xliff():
         print("No Excel file selected. Exiting.")
 
 
+# Function to handle single XLIFF to Excel conversion
 def select_xliff_to_excel():
     xliff_file_path = filedialog.askopenfilename(
         title="Select XLIFF File",
@@ -190,11 +174,78 @@ def select_xliff_to_excel():
     if xliff_file_path:
         logging.info("XLIFF file selected.")
         print("XLIFF file selected.")
-        xliff_to_excel(xliff_file_path)
+
+        try:
+            wb, target_language = xliff_to_excel(xliff_file_path)
+
+            # Set default output file name using target-language
+            default_output_filename = f"Excel to xlf {target_language}.xlsx"
+            output_file_path = filedialog.asksaveasfilename(
+                title="Save Excel File As",
+                defaultextension=".xlsx",
+                filetypes=[("Excel files", "*.xlsx"), ("All Files", "*.*")],
+                initialfile=default_output_filename
+            )
+
+            if output_file_path:
+                wb.save(output_file_path)
+                logging.info(f"XLIFF converted to Excel and saved at {output_file_path}")
+                print(f"XLIFF converted to Excel and saved at {output_file_path}")
+            else:
+                logging.info("File save operation was cancelled.")
+                print("File save operation was cancelled.")
+
+        except Exception as e:
+            logging.error(f"An error occurred during the XLIFF to Excel conversion: {e}")
+            print(f"An error occurred during the XLIFF to Excel conversion: {e}")
+            raise
+
     else:
         logging.info("No XLIFF file selected. Exiting.")
         print("No XLIFF file selected. Exiting.")
 
+
+# New function to handle multiple XLIFF to Excel conversion using the refactored function
+def multiple_xliff_to_excel():
+    xliff_files = filedialog.askopenfilenames(
+        title="Select Multiple XLIFF Files",
+        filetypes=[("XLIFF files", "*.xlf"), ("All Files", "*.*")]
+    )
+
+    if not xliff_files:
+        logging.info("No files selected. Exiting.")
+        return
+
+    # Ask the user to select a base folder for saving the Excel files
+    base_folder = filedialog.askdirectory(title="Select Base Folder for Output")
+    if not base_folder:
+        logging.info("No base folder selected. Exiting.")
+        return
+
+    for xliff_file in xliff_files:
+        try:
+            # Use the refactored xliff_to_excel function to get the workbook and language
+            wb, target_language = xliff_to_excel(xliff_file)
+
+            # Create a folder for the target language if it doesn't exist
+            folder_path = os.path.join(base_folder, target_language)
+            os.makedirs(folder_path, exist_ok=True)
+
+            # Define the output file path for the Excel file in the respective folder
+            excel_filename = f"Excel to xlf {target_language}.xlsx"
+            output_file_path = os.path.join(folder_path, excel_filename)
+
+            # Save the Excel file
+            wb.save(output_file_path)
+            logging.info(f"XLIFF converted to Excel and saved at {output_file_path}")
+            print(f"XLIFF converted to Excel and saved at {output_file_path}")
+
+        except Exception as e:
+            logging.error(f"An error occurred while processing {xliff_file}: {e}")
+            print(f"An error occurred while processing {xliff_file}: {e}")
+
+    logging.info("Multiple files processed successfully.")
+    messagebox.showinfo("Success", "All files have been processed and saved.")
 
 def select_two_files():
     # Create a new window for selecting files
@@ -243,7 +294,22 @@ def select_two_files():
         try:
             # Convert source XLIFF to Excel and save it
             logging.info("Converting source language file to Excel...")
-            xliff_to_excel(source_file_path)
+            wb, target_language = xliff_to_excel(source_file_path)
+
+            # Prompt the user to save the Excel file
+            output_file_path = filedialog.asksaveasfilename(
+                title="Save Source Language Excel File As",
+                defaultextension=".xlsx",
+                filetypes=[("Excel files", "*.xlsx"), ("All Files", "*.*")],
+                initialfile=f"{target_language}_source.xlsx"
+            )
+
+            if output_file_path:
+                wb.save(output_file_path)
+                logging.info(f"Source Excel file saved at {output_file_path}")
+            else:
+                logging.info("File save operation was cancelled.")
+                return  # Exit if the user cancels the save operation
 
             # Now process the English file, but don't save it, just extract Target values
             logging.info("Extracting Target values from the English file...")
@@ -259,7 +325,7 @@ def select_two_files():
                     target_text = trans_unit.find("target").text if trans_unit.find("target") is not None else ""
                     english_translation_map[id_value] = target_text  # Store the ID-to-Target mapping
 
-            # Use the already saved output file path for the source Excel
+            # Load the previously saved source Excel file
             logging.info(f"Loading the previously saved Excel file from {output_file_path}...")
             if not output_file_path:
                 raise ValueError("No output file found. Please save the Excel file first.")
@@ -330,16 +396,21 @@ def select_two_files():
     close_btn.pack(pady=10)
 
 
+# GUI update to add the new button
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Excel to XLIFF Converter")
-    root.geometry("300x150")
+    root.geometry("300x200")
 
     btn_excel_to_xliff = tk.Button(root, text="Excel to XLIFF", command=select_excel_to_xliff, width=20)
     btn_excel_to_xliff.pack(pady=10)
 
     btn_xliff_to_excel = tk.Button(root, text="XLIFF to Excel", command=select_xliff_to_excel, width=20)
     btn_xliff_to_excel.pack(pady=10)
+
+    btn_multiple_xliff_to_excel = tk.Button(root, text="Multiple Files XLIFF to Excel", command=multiple_xliff_to_excel,
+                                            width=20)
+    btn_multiple_xliff_to_excel.pack(pady=10)
 
     btn_select_files = tk.Button(root, text="Feedback file automation", command=select_two_files, width=20)
     btn_select_files.pack(pady=10)
